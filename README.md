709f485c1a2816cdff8c80b8677dc24e67e7b8ea
Sora Liu <soraliu.dev@gmail.com>
2020-05-29 18:52:43 +0800

    feat(init): init project

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # zsh-ghf
+
 Manage knowledge fragments by github issues
+
+# Requirements
+
+- curl
+- [jq](https://stedolan.github.io/jq/download)
+- [translate-shell](https://github.com/soimort/translate-shell#installation) (optional)
+
+In osx, you can exec the following codes
+
+```bash
+brew install jq
+brew install translate-shell
+```
+
+# Installation
+
+```bash
+git clone https://github.com/soraliu/zsh-ghf.git $ZSH_CUSTOM/plugins/zsh-ghf
+```
+
+```bash
+# ~/.zshrc
+
+plugins=(
+  # ...
+  zsh-ghf
+  # ...
+)
+
+# ~/.ghfrc
+# substitute ${user} ${token} with your own configuration
+export ZSH_GHF_API_URL=https://${user}:${token}@api.github.com/repos/soraliu
+export ZSH_GHF_REPO_NAME_FRAGMENT=dev-infra
+export ZSH_GHF_REPO_NAME_LANG_LEARNING=trans
+```
+
+# Usage
+
+## push
+
+```bash
+push -t shell 'find . -type d -name "${dirname:-dir}"'
+```
+
+## dict
+
+```bash
+dict -t fruit apple
+```
+
+## tozh
+
+```bash
+tozh -t 'oral english' -t 'community' "I'v got it."
+```
---
+++ zsh-ghf.zsh
@@ -0,0 +1,134 @@
+[[ -e ~/.ghfrc ]] && source ~/.ghfrc
+
+0=${(%):-%N}
+typeset -g ZSH_GHF_VERSION=$(<"${0:A:h}"/.version)
+typeset -g ZSH_GHF_REVISION=$(<"${0:A:h}"/.revision-hash)
+
+pre-check() {
+  if [[ $ZSH_GHF_API_URL == "" ]]; then
+    echo 'please configure $ZSH_GHF_API_URL in ~/.ghfrc'
+    echo 'TL;DR https://github.com/soraliu/zsh-ghf'
+    exit 1
+  fi
+
+  if [[ $ZSH_GHF_REPO_NAME_FRAGMENT == "" ]]; then
+    echo 'please configure $ZSH_GHF_REPO_NAME_FRAGMENT in ~/.ghfrc'
+    echo 'TL;DR https://github.com/soraliu/zsh-ghf'
+    exit 1
+  fi
+
+  if ! command -v jq 1>/dev/null 2>&1; then
+    echo 'please install jq'
+    echo 'TL;DR https://github.com/soraliu/zsh-ghf'
+    exit 1
+  fi
+
+  if ! command -v curl 1>/dev/null 2>&1; then
+    echo 'please install curl'
+    echo 'TL;DR https://github.com/soraliu/zsh-ghf'
+    exit 1
+  fi
+}
+
+ZSH_GHF_REPO_NAME=${ZSH_GHF_REPO_NAME_FRAGMENT}
+
+ghf() {
+  set -e
+  pre-check
+
+  usage() {
+    echo "usage:
+      [-t | --tag]
+      [-c | --code]
+      [-h]"
+  }
+
+  comment=
+  tags=
+  api=${ZSH_GHF_API_URL}/${ZSH_GHF_REPO_NAME}/issues
+  today=$(date +"%Y-%m-%d")
+
+  while [ "$1" != "" ]; do
+    case $1 in
+      -t | --tag ) shift
+        if ! echo $tags | grep "\`$1\`" 1>/dev/null 2>&1; then
+          tags="$tags\`$1\` "
+        fi
+        ;;
+      -c | --code )
+        is_code=true
+        ;;
+      -h | --help )
+        usage
+        exit
+        ;;
+      * )
+        if [[ $comment == "" ]]; then
+          comment="$1"
+        else
+          comment="$comment
+
+$1"
+        fi
+    esac
+    shift
+  done
+
+  if command -v wrapper 1>/dev/null 2>&1; then
+    comment=$(wrapper $comment)
+  fi
+
+  # if is_code, wrap comment with ```
+  if [[ $is_code == true ]]; then
+    comment="\`\`\`
+$comment
+\`\`\`"
+  fi
+
+
+  # get pr number if exist
+  id=$(curl -s $api | jq ".[] | select(.title == \"${today}\") | .number")
+
+  if [[ "" == "$id" ]]; then
+    # create issue
+    id=$(curl -s -X POST --data '{"title": "'$today'"}' $api | jq '.number')
+  fi
+
+  if [[ $tags == "" ]]; then
+    body="${comment}"
+  else
+    body="${tags}
+${comment}"
+  fi
+  echo $(curl -s -X POST --data '{"body": '"$(jq -aRs . <<< ${body})"'}' $api/$id/comments | jq -r '.html_url')
+  echo "$body"
+}
+
+dict() {
+  wrapper() {
+    trans -no-ansi $1
+  }
+
+  ZSH_GHF_REPO_NAME=${ZSH_GHF_REPO_NAME_LANG_LEARNING}
+  ghf -c $@
+}
+
+tozh() {
+  wrapper() {
+    trans -no-ansi en:zh $1
+  }
+
+  ZSH_GHF_REPO_NAME=${ZSH_GHF_REPO_NAME_LANG_LEARNING}
+  ghf -c $@
+}
+
+toen() {
+  wrapper() {
+    trans -no-ansi zh:en $1
+  }
+
+  ZSH_GHF_REPO_NAME=${ZSH_GHF_REPO_NAME_LANG_LEARNING}
+  ghf -c $@
+}
+
+alias push="ghf"
